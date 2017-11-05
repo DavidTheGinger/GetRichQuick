27abaecc11573e947e86c128fbb17457f3098ff6
limy97 <32105571+limy97@users.noreply.github.com>
2017-11-05 09:21:03 +0800

    create five spaces for puzzles, made two puzzles, create camera movement
    create five spaces for puzzles, made two puzzles, create camera movement

---
+++ Assets/Scripts/rewire connections/lightDecision.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightDecision : MonoBehaviour {
+    public GameObject[] wrongWires;
+    public GameObject[] correctWires;
+    public GameObject[] veryWrong;
+    public GameObject outlet;
+    private Color wrongColor = Color.red;
+    private Color correctColor = Color.green;
+    private Color originalColor = Color.yellow;
+    private Renderer rend;
+    private bool isOccupied;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        rend.material.color = originalColor;
+        isOccupied = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        isOccupied = false;
+		for (int i = 0; i < wrongWires.Length; i++)
+        {
+            if (wrongWires[i].transform.position == outlet.transform.position)
+            {
+                rend.material.color = wrongColor;
+                isOccupied = true;
+            }
+        }
+        for (int i = 0; i < correctWires.Length; i++)
+        {
+            if (correctWires[i].transform.position == outlet.transform.position)
+            {
+                rend.material.color = correctColor;
+                isOccupied = true;
+            }
+        }
+        if (!isOccupied)
+        {
+            rend.material.color = originalColor;
+        }
+	}
+}
