183bb7132f394d3681349cf4041c9a4147e20c6f
limy97 <32105571+limy97@users.noreply.github.com>
2017-11-08 19:35:58 +0800

    Eve: Fixed UI

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
@@ -16,15 +17,16 @@ public class Timer : MonoBehaviour
 
     private int i;
     private bool b;
+    private float t;
 
     // Use this for initialization
     void Start()
     {
 
         Ending.SetActive(false);
-
+        t = startTime;
         i = 0;
-
+        //startTime = 300f;
         b = false;
 
     }
@@ -44,12 +46,39 @@ public class Timer : MonoBehaviour
 
     }
 
+    public void RestartE()
+    {
+        
+        Ending.SetActive(false);
+        Time.timeScale = 1;
+        t = 300f;
+        startTime = Time.time + t;
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void MainMenuE(string mainmenu)
+    {
+        SceneManager.LoadScene(mainmenu);
+        Ending.SetActive(false);
+        Time.timeScale = 1;
+        t = 300f;
+    }
+
+    public void ExitingGameE()
+    {
+        Application.Quit();
+        Ending.SetActive(false);
+        Time.timeScale = 1;
+        t = 300f;
+    }
+
     // Update is called once per frame
     void Update()
     {
 
         //Set timer
-        float t = startTime - Time.time;
+       // t = startTime - Time.time;
+        t -= Time.deltaTime;
 
         string minutes = ((int)t / 60).ToString();
         string seconds = (t % 60).ToString("f2");
@@ -73,6 +102,7 @@ public class Timer : MonoBehaviour
         {
             t = 0;
             Ending.SetActive(true);
+            Time.timeScale = 0;
 
             if (i <= score)
             {
