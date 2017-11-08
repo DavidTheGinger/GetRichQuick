abe19edb46090e51f081ef29b733437e97c3a7b1
Eve Dai <daiy2@rpi.edu>
2017-11-08 04:18:56 -0500

    timer start to blink (weirdly) when left time <= 15s

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -3,29 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Timer : MonoBehaviour {
+public class Timer : MonoBehaviour
+{
 
     public Text timertext;
     public Text Score;
     public Color NewColor;
     public GameObject Ending;
+    public GameObject Blink;
     public int score = 50;
-    private int i;
-
-
     public float startTime;
 
-	// Use this for initialization
-	void Start () {
+    private int i;
+    private bool b;
+
+    // Use this for initialization
+    void Start()
+    {
 
         Ending.SetActive(false);
 
         i = 0;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+        b = false;
+
+    }
+
+    //timer blink
+    void blink()
+    {
+
+        if (Blink.activeInHierarchy)
+        {
+            Blink.SetActive(false);
+        }
+        else
+        {
+            Blink.SetActive(true);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Set timer
         float t = startTime - Time.time;
 
         string minutes = ((int)t / 60).ToString();
@@ -33,11 +56,19 @@ public class Timer : MonoBehaviour {
 
         timertext.text = minutes + ":" + seconds;
 
+        //Timer turns red when time is less than 30s
         if (t < 31f)
         {
             timertext.color = NewColor;
         }
 
+        //Timer starts blink when time is less than 15s
+        if (t < 15f)
+        {
+                InvokeRepeating("blink", 0f, 10f);
+        }
+
+        //Show ending scene and start counter
         if (t <= 0)
         {
             t = 0;
@@ -46,8 +77,9 @@ public class Timer : MonoBehaviour {
             if (i <= score)
             {
                 Score.text = "$ " + i;
-                i = i+10;
+                i = i + 10;
             }
         }
-	}
+    }
 }
+    
